3ac71f4dd92bb38ef54557e6b5b5dcf974318d11
Dana Kendybayeva <47076267+danakendybayeva@users.noreply.github.com>
2023-06-24 17:25:18 +0600

    предметы работают

--- src/app/user/_services/user.service.ts
+++ src/app/user/_services/user.service.ts
@@ -3,50 +3,53 @@ import { HttpClient } from '@angular/common/http';
 
 import { environment } from '../../../environments/environment';
 import { User } from '../_models/user';
-import {Status} from '../../interfaces/services/util.service';
-import {map} from 'rxjs/operators';
-import {Question} from '../../interfaces/services/projects/reads.service';
+import { Status } from '../../interfaces/services/util.service';
+import { map } from 'rxjs/operators';
+import { Question } from '../../interfaces/services/projects/reads.service';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-    getAll() {
-        return this.http.get<User[]>(`${environment.apiUrl}/users`);
-    }
+  getAll() {
+    return this.http.get<User[]>(`${environment.apiUrl}/users`);
+  }
 
-    getById(id: number) {
-        return this.http.get<User>(`${environment.apiUrl}/users/${id}`);
-    }
+  getById(id: number) {
+    return this.http.get<User>(`${environment.apiUrl}/users/${id}`);
+  }
 
-    getAdmin(): Promise<Status> {
-        return this.http.get(`${environment.apiUrl}/api/auth/user/role/super/admin`)
-            .toPromise()
-            .then(response => response as Status)
-            .catch();
-    }
+  getAdmin(): Promise<Status> {
+    return this.http
+      .get(`${environment.apiUrl}/api/auth/user/role/super/admin`)
+      .toPromise()
+      .then((response) => response as Status)
+      .catch();
+  }
 
-    async isAdmin(): Promise<boolean> {
-        const res = await this.getAdmin();
-        return (res.status === 1) ? true : false;
-    }
+  async isAdmin(): Promise<boolean> {
+    const res = await this.getAdmin();
+    return res.status === 1 ? true : false;
+  }
 
-    async roleAccount(role: String[]): Promise<boolean> {
-        if (role.length === 0) {
-            return true;
-        }
-        const res = await this.http.post<Status>(`${environment.apiUrl}/api/auth/account/islogging`, role)
-            .toPromise()
-            .then(response => response as Status)
-            .catch();
-        return (res.status === 1) ? true : false;
+  async roleAccount(role: String[]): Promise<boolean> {
+    if (role.length === 0) {
+      return true;
     }
+    const res = await this.http
+      .post<Status>(`${environment.apiUrl}/api/auth/account/islogging`, role)
+      .toPromise()
+      .then((response) => response as Status)
+      .catch();
+    return res.status === 1 ? true : false;
+  }
 
-    async getRolesAccount(): Promise<string> {
-        const roles = await this.http.get(`${environment.apiUrl}/api/auth/user/role`)
-            .toPromise()
-            .then(response => response as Status)
-            .catch();
-        return (roles.status === 1) ? roles.value : '';
-    }
+  async getRolesAccount(): Promise<string> {
+    const roles = await this.http
+      .get(`${environment.apiUrl}/api/auth/user/role`)
+      .toPromise()
+      .then((response) => response as Status)
+      .catch();
+    return roles.status === 1 ? roles.value : '';
+  }
 }
